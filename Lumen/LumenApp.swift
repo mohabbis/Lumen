93fd63cbb50e457cd914b0f3eb95dd429969b4dc
Muhammad Rafiq <101276427+mohabbis@users.noreply.github.com>
2026-05-28 22:55:55 -0500

    Initial Commit

---
+++ Lumen/LumenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumenApp.swift
+//  Lumen
+//
+//  Created by Muhammad Rafiq on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct LumenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
