80781b67fce6942d5a0ba8f00f97a9139bd1c10c
Muhammad Rafiq <101276427+mohabbis@users.noreply.github.com>
2026-06-10 11:13:12 -0500

    Set production site URL for Car Wash Guys

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://carwashguys.us",
+	integrations: [mdx(), sitemap()],
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true,
+		},
+	}),
+});
